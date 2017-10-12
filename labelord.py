3c2b692e731f9b81ab59617a22193c87c9291a71
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-10-12 20:36:56 +0200

    MI-PYT - ukol 01: základy run update, happy `tests/test_run_basic.py` and little bit happy `tests/test_run_update.py`

--- labelord.py
+++ labelord.py
@@ -16,6 +16,7 @@ import configparser
 @click.option('-t','--token',envvar='GITHUB_TOKEN')
 def cli(ctx,config_path,token):
     config = configparser.ConfigParser()
+    config.optionxform = lambda option: option
 
     if config_path:
         config.read(config_path)
@@ -36,6 +37,7 @@ def cli(ctx,config_path,token):
     session.auth = token_auth
 
     ctx.obj['session'] = session
+    ctx.obj['config'] = config
 
 
 @cli.command()
@@ -85,10 +87,38 @@ def list_labels(ctx,slug):
 
 @cli.command()
 @click.pass_context
-def run(ctx):
-    # TODO: Add required options/arguments
-    # TODO: Implement the 'run' command
-    ...
+@click.argument('mode')
+@click.option('-v','--verbose', default=False, is_flag=True)
+@click.option('-q','--quiet', default=False, is_flag=True)
+def run(ctx,mode,verbose,quiet):
+    config = ctx.obj['config']
+    session = ctx.obj['session']
+    if not 'labels' in config:
+        print('No labels specification has been found')
+        exit(6)
+    if not 'repos' in config:
+        print('No repositories specification has been found')
+        exit(7)
+
+    updated_repos = 0
+    for repo in config['repos']:
+        if not config['repos'][repo] in ['on', 'true']:
+            continue
+
+        original_labels = session.get('https://api.github.com/repos/{}/labels?per_page=100&page=1'.format(repo)).json()
+        updated_repos = updated_repos + 1
+        for label in config['labels']:
+            label_json={'name': label,'color':config['labels'][label]}
+            match = [l for l in original_labels if label == l['name'] ]
+            if len(match) == 0:
+                r = session.post('https://api.github.com/repos/{}/labels'.format(repo), json=label_json)
+            else:
+                if match[0]['color'] != config['labels'][label]:
+                    label_url = 'https://api.github.com/repos/{}/labels/{}'.format(repo,label)
+                    r = session.patch(label_url,json=label_json)
+
+    print ('SUMMARY: {} repo(s) updated successfully'.format(updated_repos))
+
 
 
 if __name__ == '__main__':

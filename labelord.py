af9c9436f880a338482dc4bd028fa1cb4ec36a99
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-10-15 11:25:34 +0200

    MI-PYT - ukol 01: implementovaný run update (bez správnej implementácie prepínačov --verbose, --dry-run; `tests/test_run_update.py` happy

--- labelord.py
+++ labelord.py
@@ -39,13 +39,8 @@ def cli(ctx,config_path,token):
     ctx.obj['session'] = session
     ctx.obj['config'] = config
 
-
-@cli.command()
-@click.pass_context
-def list_repos(ctx):
-    # TODO: Add required options/arguments
-    # TODO: Implement the 'list_repos' command
-
+def retrieve_repos(ctx):
+    repos = []
     url = 'https://api.github.com/user/repos?per_page=100&page=1'
 
     while url:
@@ -55,18 +50,24 @@ def list_repos(ctx):
             print("GitHub: ERROR {} - {}".format(r.status_code,r.json()['message']))
             exit(4)
         for repo in r.json():
-            print(repo['full_name'])
+            repos.append(repo)
 
         if 'next' in r.links:
             url = r.links['next']['url']
         else:
             url = None
-
+    return repos
 
 @cli.command()
 @click.pass_context
-@click.argument('slug')
-def list_labels(ctx,slug):
+def list_repos(ctx):
+    # TODO: Add required options/arguments
+    # TODO: Implement the 'list_repos' command
+    for repo in retrieve_repos(ctx):
+        print(repo['full_name'])
+
+def retrieve_labels(ctx,slug):
+    labels = []
     url = 'https://api.github.com/repos/{}/labels?per_page=100&page=1'.format(slug)
     session = ctx.obj['session']
     while url:
@@ -78,11 +79,19 @@ def list_labels(ctx,slug):
             print("GitHub: ERROR {} - {}".format(r.status_code,r.json()['message']))
             exit(4)
         for label in r.json():
-            print('#{} {}'.format(label['color'], label['name']))
+            labels.append(label)
         if 'next' in r.links:
             url = r.links['next']['url']
         else:
             url = None
+    return labels
+
+@cli.command()
+@click.pass_context
+@click.argument('slug')
+def list_labels(ctx,slug):
+    for label in retrieve_labels(ctx,slug):
+        print('#{} {}'.format(label['color'], label['name']))
 
 
 @cli.command()
@@ -90,7 +99,9 @@ def list_labels(ctx,slug):
 @click.argument('mode')
 @click.option('-v','--verbose', default=False, is_flag=True)
 @click.option('-q','--quiet', default=False, is_flag=True)
-def run(ctx,mode,verbose,quiet):
+@click.option('-a','--all-repos', 'all_repos', default=False, is_flag=True)
+@click.option('-d','--dry-run', 'dry_run', default=False, is_flag=True)
+def run(ctx,mode,verbose,quiet,all_repos,dry_run):
     config = ctx.obj['config']
     session = ctx.obj['session']
     if not 'labels' in config:
@@ -101,23 +112,56 @@ def run(ctx,mode,verbose,quiet):
         exit(7)
 
     updated_repos = 0
-    for repo in config['repos']:
-        if not config['repos'][repo] in ['on', 'true']:
-            continue
+    update_errors = 0
+    repos = []
+    if 'repos' in config:
+        repos = [repo for repo in config['repos'] if config['repos'][repo].lower() in ['on', 'true', '1']]
 
-        original_labels = session.get('https://api.github.com/repos/{}/labels?per_page=100&page=1'.format(repo)).json()
+    if all_repos:
+        repos = [repo['full_name'] for repo in retrieve_repos(ctx)]
+
+    labels = [(label,config['labels'][label]) for label in config['labels']]
+    if 'others' in config and 'template-repo' in config['others']:
+        labels = [(label['name'],label['color']) for label in retrieve_labels(ctx,config['others']['template-repo'])]
+
+    for repo in repos:
+        original_labels_r = session.get('https://api.github.com/repos/{}/labels?per_page=100&page=1'.format(repo))
+        if original_labels_r.status_code != 200:
+            update_errors += 1
+            print("ERROR: LBL; {}; {} - {}".format(repo,original_labels_r.status_code,original_labels_r.json()['message']))
+            continue
+        original_labels = original_labels_r.json()
         updated_repos = updated_repos + 1
-        for label in config['labels']:
-            label_json={'name': label,'color':config['labels'][label]}
+        for label,color in labels:
+            label_json={'name': label,'color':color}
             match = [l for l in original_labels if label == l['name'] ]
             if len(match) == 0:
+                # TODO: outputs
+                if dry_run:
+                    continue
                 r = session.post('https://api.github.com/repos/{}/labels'.format(repo), json=label_json)
+                if verbose:
+                    print('[ADD][SUC] {}; {}; {}', repo, label, color)
+                if r.status_code != 201:
+                    update_errors += 1
+                    print("ERROR: ADD; {}; {}; {}; {} - {}".format(repo,label,color,r.status_code,r.json()['message']))
             else:
-                if match[0]['color'] != config['labels'][label]:
+                # TODO: outputs
+                if dry_run:
+                    continue
+                if match[0]['color'] != color:
                     label_url = 'https://api.github.com/repos/{}/labels/{}'.format(repo,label)
                     r = session.patch(label_url,json=label_json)
+                    if r.status_code != 200:
+                        update_errors += 1
+                        print("ERROR: UPD; {}; {}; {}; {} - {}".format(repo,label,color,r.status_code,r.json()['message']))
 
-    print ('SUMMARY: {} repo(s) updated successfully'.format(updated_repos))
+    if update_errors == 0:
+        print ('SUMMARY: {} repo(s) updated successfully'.format(updated_repos))
+    else:
+        print ('SUMMARY: {} error(s) in total, please check log above'.format(update_errors))
+        exit(10)
+
 
 
 

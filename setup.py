39eed1b1cab4dc2d276f13fba82f69a155ad6a9f
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-10-28 12:40:49 +0200

    MI-PYT - ukol 04: added long description (read from README.md), added classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,23 @@
 from setuptools import setup
 
+with open('README.md') as f:
+    long_description = ''.join(f.readlines())
 
 setup(
     name='labelord_kravemir',
     version='0.2',
     description='CTU FIT - MI-PYT: homework labelord packaging',
+    long_description=long_description,
     author='Miroslav Kravec',
     author_email='kravemir@fit.cvut.cz',
     license='Creative Commons Legal Code',
     url='https://github.com/kravemir/MI-PYT-ukol-01',
     py_modules=['labelord'],
+    classifiers = [
+        'Intended Audience :: Other Audience',
+        'Framework :: Flask',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+    ]
 )

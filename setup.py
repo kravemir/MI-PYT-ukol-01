bec6583eaed613b209084dbc78beff08df29025d
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-10-28 18:24:21 +0200

    MI-PYT - ukol 04: added more metadata for 'tests_module/test_distribution.py::test_package_info'

--- setup.py
+++ setup.py
@@ -5,16 +5,19 @@ with open('README.md') as f:
 
 setup(
     name='labelord_kravemir',
-    version='0.2',
+    version='0.3-SNAPSHOT',
     description='CTU FIT - MI-PYT: homework labelord packaging',
     long_description=long_description,
     author='Miroslav Kravec',
     author_email='kravemir@fit.cvut.cz',
+    keywords='homework,fit ctu,mi-pyt',
     license='Creative Commons Legal Code',
     url='https://github.com/kravemir/MI-PYT-ukol-01',
     py_modules=['labelord','github','cli','web'],
     classifiers = [
         'Intended Audience :: Other Audience',
+        'Environment :: Console',
+        'Environment :: Web Environment',
         'Framework :: Flask',
         'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
         'Programming Language :: Python',

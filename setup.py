4d41d997727a37914805914a84d81d247b7c70c2
Miroslav Kravec <kravec.miroslav@gmail.com>
2017-10-28 12:16:34 +0200

    MI-PYT - ukol 04: initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='labelord_kravemir',
+    version='0.2',
+    description='CTU FIT - MI-PYT: homework labelord packaging',
+    author='Miroslav Kravec',
+    author_email='kravemir@fit.cvut.cz',
+    license='Creative Commons Legal Code',
+    url='https://github.com/kravemir/MI-PYT-ukol-01',
+    py_modules=['labelord'],
+)
